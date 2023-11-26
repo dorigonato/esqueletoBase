b056f9ef649fb81e95273e480da83697e4141455
arthurdabela <arthurdabela@gmail.com>
2023-11-26 01:17:58 -0400

    Funções para página de rastreamento

--- funcoesAll.js
+++ funcoesAll.js
@@ -46,7 +46,7 @@ function validateForm() {
   }
 }
 
-function rastrear() {
+function tipo_rastreio() {
   var tipoRastreio = document.querySelector('#tipo-rastreamento');
   var option = tipoRastreio.value;
   var labelRastreio = document.querySelector('#label-rastreamento');
@@ -56,4 +56,22 @@ function rastrear() {
   if (option == "numero-pedido") {
     labelRastreio.innerHTML = "Número do pedido:";
   } 
+}
+
+function rastrear(){
+  var codigoRastreio = document.querySelector("#numero-rastreamento-pedido").value;
+  var dadosPedido = document.querySelector(".pedido");
+  var footer = document.querySelector('footer');
+  if (codigoRastreio == "123") {
+    dadosPedido.innerHTML = "<h2>Detalhes do Pedido:</h2><ul>    <li>Número de pedido: 123456.</li><br><li>Data de Compra: 20 de novembro de 2023.</li><br><li>Endereço de entrega: Rua João da Silva, N° 1030 - Centro - Cuiabá - MT.</li><br><li>Status de entrega: Saiu para entrega.</li></ul>"
+    footer.classList.add("rastreio");
+    }
+  else if (codigoRastreio == "") {
+    dadosPedido.innerHTML = "<h2>Número não preenchido!";
+    footer.classList.remove("rastreio");
+  }
+  else{
+    dadosPedido.innerHTML = "<h2>Pedido não encontrado!</h2>";
+    footer.classList.remove("rastreio");
+  }
 }
